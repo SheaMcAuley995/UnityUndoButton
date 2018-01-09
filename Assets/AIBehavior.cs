f79bfddef446dca207a1c6216a809181fad6aa6a
Shea McAuley <s179049@students.aie.edu.au>
2018-01-08 16:23:00 -0800

    Add project files

---
+++ Assets/AIBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class AIBehavior : MonoBehaviour {
+
+    GameObject player;
+   // Vector3 offset;
+    public Rigidbody rbs;
+    NavMeshAgent agent;
+    public float speed = 8;
+    
+    // Use this for initialization
+    void Start () {
+        
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+        //offset = transform.position - player.transform.position;
+        player = FindObjectOfType<Navigation>().gameObject;
+        rbs = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //transform.position = player.transform.position;
+
+        // Vector3 move = player.transform.position - transform.position;
+        //rbs.AddForce
+        // rbs.AddForce(move.normalized * speed * Time.deltaTime);
+        agent.destination = player.transform.position;
+       
+    }
+}
---
+++ Assets/Navigation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float speed;
+    public float jumpSpeed;
+
+    private bool grounded = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.tag == "LevelasFloor")
+        {
+            grounded = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+ 
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+        if (Input.GetKey(KeyCode.Space) && grounded == true)
+        {
+            Debug.Log("Jump");
+            //Vector3 jumping = new Vector3(0, )
+            move.y += jumpSpeed;
+            grounded = false;
+        }
+        move.y -= 20.0f * Time.deltaTime;
+
+        
+        rb.AddForce(move * speed * Time.deltaTime);
+		
+	}
+}
